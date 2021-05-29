9aa8bb0b3ff5c470c98a3e2c24ddb6a59b3b4c78
jevinviradiya <84986617+jevinviradiya@users.noreply.github.com>
2021-05-29 21:56:35 +0530

    Add files via upload

---
+++ Day 2/List.py
@@ -0,0 +1,9 @@
+list0 = ["jevin", 11, 22, "viradiya", 55, 99]
+
+print("list1[5] = ", list0[5])
+
+print("list0[2:5] = ", list0[0:5])
+
+print("list0[3:] = ", list0[3:])
+
+print("list0[:6] = ", list0[:4])
